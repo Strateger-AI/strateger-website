5c6466c0d6ef10863586f6d99094bc559bdcc78c
masjad611 <muhammad.asjad@strateger.ai>
2025-11-06 19:32:12 +0500

    temp remove workflow

--- src/data/main.data.ts
+++ src/data/main.data.ts
@@ -252,7 +252,7 @@ export const NAV_ITEMS = [
       {
         title: "Entertainment",
         href: "/industries/entertainment",
-        desc: "Were Fun Meets Innovation",
+        desc: "Where Fun Meets Innovation",
         icon: FaGamepad,
         subcategories: [],
       },
